67c0410282dbf7fb648274bce6c2d9f7808dd294
Altiberg.Adrien <altibergadrien2016@gmail.com>
2023-12-07 19:46:12 -0300

    Commit end

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+// animação da pagina de formulario enviado
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.container').classList.add('aparecer');
+});
+
+
+
